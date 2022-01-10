c662df054a72acac08bcefee1142ef077cda56c2
George991808 <gellis7@gmail.com>
2022-01-10 18:50:46 +0800

    stying,initital strucutre, first start button added

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+
+var startButtonEl = $('#start-btn');
+
+
+
+// define questions
+var Q1="What is Life?"
+var Q2="What is 2?"
+var Q3="What is 3?"
+var Q4="What is 4?"
+
+
+// Define wrong answers
+var Wrong1=["a","b","c"]
+var Wrong2=["a","b","c"]
+var Wrong3=["a","b","c"]
+var Wrong4=["a","b","c"]
+
+// Define correct answers
+var Correct1="d"
+var Correct2="d"
+var Correct3="d"
+var Correct4="d"
+
+
+
+
+// Click event causes alert light start toggle
+startButtonEl.on('click', function () {
+
+});
+
